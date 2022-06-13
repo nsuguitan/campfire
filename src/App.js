67f603cf3e6796f847f27b08f0727fcfba48de86
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-06-13 15:07:09 -0400

    Browser router added for sign-in, newsfeed and profile pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import Newsfeed from "./pages/Newsfeed";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<SignIn />} />
+        <Route exact path="/Newsfeed" element={<Newsfeed />} />
+        <Route exact path="/Profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Newsfeed.js
@@ -0,0 +1,8 @@
+const Newsfeed = () => {
+  return (
+    <div>
+      <h1>This is the newsfeed page</h1>
+    </div>
+  );
+};
+export default Newsfeed;
