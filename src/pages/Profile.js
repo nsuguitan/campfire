f6410ce34759daeb3091ad7897023cf2806bce07
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-06-16 15:16:08 -0400

    minor page stlying

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,21 @@
 //import Postcard from "../components/Postcard/Postcard";
 import profilePicture from '../assets/profilePic.jpg';
+import Image1 from '../assets/gridPhoto1.jpg';
+import Image2 from '../assets/gridPhoto2.jpg';
+import Image3 from '../assets/gridPhoto3.jpg';
+import Image4 from '../assets/gridPhoto4.jpg';
+import Image5 from '../assets/gridPhoto5.jpg';
+import Image6 from '../assets/gridPhoto6.jpg';
+import Image7 from '../assets/gridPhoto7.jpg';
+import Image8 from '../assets/gridPhoto8.jpg';
+import Image9 from '../assets/gridPhoto9.jpg';
+import Image10 from '../assets/gridPhoto10.jpg';
+
 
 const Profile = () => {
   return (
     <div className='pageContainer'>
-      <div><h1>chelmerr</h1></div>
+      <div className='bioContainer'><h1>chelmerr</h1></div>
       <div className='topProfileContainer'>
         <div className='profilePicContainer'>
           <img src={profilePicture} alt='' className='profilePic' />
@@ -29,6 +40,27 @@ const Profile = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className='bioContainer'>
+        <div className='fullName'>
+          <h2>Chelsea Merrill</h2>
+        </div>
+        <div className='bio'>
+          <p>Former child, future Sith, and current survior of natual selection</p>
+        </div>
+      </div>
+      <div className='imageGridContainer'>
+        <div className='singleGridPhotoContainter'><img src={Image1} className='test'/></div>
+        <img src={Image1} className='singleGridPhoto'/>
+        <img src={Image2} className='singleGridPhoto'/>
+        <img src={Image3} className='singleGridPhoto'/>
+        <img src={Image4} className='singleGridPhoto'/>
+        <img src={Image5} className='singleGridPhoto'/>
+        <img src={Image6} className='singleGridPhoto'/>
+        <img src={Image7} className='singleGridPhoto'/>
+        <img src={Image8} className='singleGridPhoto'/>
+        <img src={Image9} className='singleGridPhoto'/>
+        <img src={Image10} className='singleGridPhoto'/>
       </div>
 
     </div>
