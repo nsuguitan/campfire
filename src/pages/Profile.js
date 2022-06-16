7d826efca3479b0f5b4419723d7a1700cf6eb1b0
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-06-16 14:15:07 -0400

    added profile picture and initial stats

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,36 @@
 //import Postcard from "../components/Postcard/Postcard";
+import profilePicture from '../assets/profilePic.jpg';
 
 const Profile = () => {
   return (
     <div className='pageContainer'>
-      <h1>This is the profile page</h1>
-      
+      <div><h1>chelmerr</h1></div>
+      <div className='topProfileContainer'>
+        <div className='profilePicContainer'>
+          <img src={profilePicture} alt='' className='profilePic' />
+        </div>
+        <div className='profileStatsContainer'>
+          <div className='roastRatingProfileContainer'>
+            <h2>Roast Rank:</h2>
+            <h2>Golden</h2>
+          </div>
+          <div className='profileDataContainer'>
+            <div className='statEntry'>
+              <h3>100</h3>
+              <h5>Posts</h5>
+            </div>
+            <div className='statEntry'>
+              <h3>200</h3>
+              <h5>Following</h5>
+            </div>
+            <div className='statEntry'>
+              <h3>300</h3>
+              <h5>Followers</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 };
