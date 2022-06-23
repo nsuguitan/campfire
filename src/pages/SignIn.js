b126b7094de4ba44bdfaf361bab21034b37f3dea
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-06-23 14:32:41 -0400

    fixed signin height

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -2,7 +2,7 @@ import SignInComp from "../components/SignIn/SignIn";
 import SignUp from "../components/SignUp/SignUp";
 const SignIn = () => {
   return (
-    <div className="pageContainer">
+    <div className="pageContainer signInPage">
       <SignInComp />
     </div>
   );
