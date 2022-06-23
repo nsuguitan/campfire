01512c9a419c32a960c56f6fdb0299a68b93af75
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-06-23 14:00:23 -0400

    sign-in page framework complete

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,6 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faGoogle,
+  faApple,
+} from "@fortawesome/free-brands-svg-icons";
 import { TextField, Box } from "@mui/material";
-import { Button } from "@mui/material";
+import { Button, Grid, Item } from "@mui/material";
 import "./SignIn.css";
 const SignInComp = () => {
   return (
@@ -8,24 +13,23 @@ const SignInComp = () => {
       <Box
         sx={{
           width: 1,
-          height: 1,
+          height: .95,
           border: 1,
           borderColor: "#212121",
           borderWidth: "3px",
         }}
       >
-        <h1>Campfire</h1>
+        <h1 style={{ marginBottom: "20px" }}>Campfire</h1>
         <div className="sign-in-input-container">
           <TextField
             fullWidth
-            className="sign-in-field"
+            sx={{ marginBottom: "10px" }}
             label="Username"
             size="small"
             variant="outlined"
           />
           <TextField
             fullWidth
-            className="sign-in-field"
             label="Password"
             size="small"
             variant="outlined"
@@ -35,7 +39,7 @@ const SignInComp = () => {
             size="medium"
             disableRipple={true}
             variant="text"
-            style={{
+            sx={{
               backgroundColor: "transparent",
               textTransform: "none",
               color: "#BEBEBE",
@@ -44,8 +48,56 @@ const SignInComp = () => {
             Forgot Password?
           </Button>
         </div>
+        <div className="login-buttons-container">
+          <div className="button-container">
+            <Button disableElevation fullWidth
+              variant="contained"
+              sx={{
+                backgroundColor: "#ee7e00",
+              }}
+            >
+              Login
+            </Button>
+          </div>
+          <div className="button-container">
+            <Button disableElevation fullWidth
+              variant="contained"
+              sx={{
+                backgroundColor: "#f14726"
+              }}
+            >
+              Create an Account
+            </Button>
+          </div>
+        </div>
+        <div>
+          <h2>- OR -</h2>
+          <h5>Login with</h5>
+        </div>
+        <div className="brand-login-container">
+          <Grid container spacing={2} id="gridContainer">
+            <Grid item>
+              <FontAwesomeIcon
+                icon={faGoogle}
+                style={{ height: "40px" }}
+              ></FontAwesomeIcon>
+            </Grid>
+            <Grid item>
+              <FontAwesomeIcon
+                icon={faFacebook}
+                style={{ height: "40px" }}
+              ></FontAwesomeIcon>
+            </Grid>
+            <Grid item>
+              <FontAwesomeIcon
+                icon={faApple}
+                style={{ height: "40px" }}
+              ></FontAwesomeIcon>
+            </Grid>
+          </Grid>
+        </div>
       </Box>
-    </div>
+    </div >
   );
 };
 
