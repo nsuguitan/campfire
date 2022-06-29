7c85c50c7043de8d8b2363da85052459e794d0ed
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-06-29 02:04:38 -0400

    authenticated user is directed to site

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,8 +1,8 @@
 import { TextField, Box, Modal } from "@mui/material";
 import { Button, Grid, Item } from "@mui/material";
 import { useState } from "react";
-import { AuthState } from "../../context/auth/AuthContext"
-import { useNavigate } from "react-router-dom"
+import { AuthState } from "../../context/auth/AuthContext";
+import { useNavigate } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUpComp = () => {
@@ -10,18 +10,18 @@ const SignUpComp = () => {
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [open, setOpen] = useState(false)
-  const [verificationCode, setVerificationCode] = useState("")
+  const [open, setOpen] = useState(false);
+  const [verificationCode, setVerificationCode] = useState("");
   const { login, signup, verify, isAuthenticated } = AuthState();
 
   const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
     width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
+    bgcolor: "background.paper",
+    border: "2px solid #000",
     boxShadow: 24,
     pt: 2,
     px: 4,
@@ -46,33 +46,40 @@ const SignUpComp = () => {
 
   const handleVerificationCode = (evt) => {
     setVerificationCode(evt.target.value);
-  }
+  };
 
   const triggerSignUp = async (event) => {
-    console.log("Triggered signup")
+    console.log("Triggered signup");
     event.preventDefault();
     //const successfulSignUp = true;
     const successfulSignUp = await signup({ username, password, name, email });
     if (successfulSignUp) {
-      console.log("After SignUp State:", isAuthenticated)
+      console.log("After SignUp State:", isAuthenticated);
       setOpen(true);
-      //const successfulLogin = await login({ username, password });
-      //setTimeout(() => { navigate("/Newsfeed"); }, 1000);
       // console.log("Come on vamanos, everybody let's go!")
     }
   };
 
   const triggerVerification = async (event) => {
     event.preventDefault();
-    console.log("Verification triggered!")
+    console.log("Verification triggered!");
     //https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CognitoIdentityServiceProvider.html#confirmSignUp-property
     let params = {
-      ClientId: '4gjkaose7v4olc8mvsd40nf11f',
+      ClientId: "4gjkaose7v4olc8mvsd40nf11f",
       ConfirmationCode: verificationCode,
-      Username: username
-    }
+      Username: username,
+    };
     const successfulVerify = await verify(params);
-
+    if (successfulVerify) {
+      console.log("Attempt login after verification");
+      const successfulLogin = await login({ username, password });
+      if (successfulLogin) {
+        console.log("successful login after verify");
+        setTimeout(() => {
+          navigate("/Newsfeed");
+        }, 1000);
+      }
+    }
   };
 
   return (
@@ -80,7 +87,7 @@ const SignUpComp = () => {
       <Box
         sx={{
           width: 1,
-          height: .95,
+          height: 0.95,
           border: 1,
           borderColor: "#212121",
           borderWidth: "3px",
@@ -124,23 +131,21 @@ const SignUpComp = () => {
             variant="outlined"
             onChange={handleEmailInput}
           />
-          <Button disableElevation fullWidth
+          <Button
+            disableElevation
+            fullWidth
             variant="contained"
             sx={{
               backgroundColor: "#ee7e00",
-              marginBottom: "10px"
+              marginBottom: "10px",
             }}
             onClick={triggerSignUp}
           >
             SignUp
-            </Button>
+          </Button>
         </div>
-
       </Box>
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-      >
+      <Modal open={open} onClose={() => setOpen(false)}>
         <Box sx={{ ...style, width: 400 }}>
           <h2> Verification Code</h2>
           <TextField
@@ -153,10 +158,9 @@ const SignUpComp = () => {
           />
           <Button onClick={triggerVerification}>Submit</Button>
         </Box>
-
       </Modal>
-    </div >
-  )
+    </div>
+  );
 };
 
 export default SignUpComp;
