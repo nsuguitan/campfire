4fdda58db988db8ff0dcd2166eaee6f7a6dbac98
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-06-29 11:35:22 -0400

    rating is mathmatical

--- src/components/Rating/rating.js
+++ src/components/Rating/rating.js
@@ -9,9 +9,20 @@ import {faSquareCaretUp} from "@fortawesome/free-regular-svg-icons";
 import { faFireFlameCurved } from "@fortawesome/free-solid-svg-icons";
 import IconButton from "@mui/material/IconButton";
 
+const Rating = () => {
+  const intialRating = 5;
+  // get from database
+  // pass it through with the photo id
+  return (
+    <div className="testingClass">
+      <Rater initialRating={intialRating} />
+    </div>
+  )
+};
+
 const Rater = ({ initialRating }) => {
   const [rating, setRating] = useState(initialRating)
-  console.log(setRating)
+  console.log(rating)
   return (
     <div className='ratingCpntainer'>
       <div className="rating-container">
@@ -27,31 +38,21 @@ const Rater = ({ initialRating }) => {
                 <FontAwesomeIcon icon={faSquareCaretUp} size="2xs" />
               </IconButton>
               <Menu {...bindMenu(popupState)}>
-                <MenuItem onClick={() => setRating((rating) => rating - 2)}>Fluffernutter</MenuItem>
-                <MenuItem onClick={() => setRating((rating) => rating - 1)}>Uncooked</MenuItem>
-                <MenuItem onClick={() => setRating((rating) => rating + 1)}>Toasted</MenuItem>
-                <MenuItem onClick={() => setRating((rating) => rating + 2)}>Roasted</MenuItem>
-                <MenuItem onClick={() => setRating((rating) => rating + 3)}>Burned</MenuItem>
+                <MenuItem onClick={() => setRating(rating - 2)}>Fluffernutter</MenuItem>
+                <MenuItem onClick={() => setRating(rating - 1)}>Uncooked</MenuItem>
+                <MenuItem onClick={() => setRating(rating + 1)}>Toasted</MenuItem>
+                <MenuItem onClick={() => setRating(rating + 2)}>Roasted</MenuItem>
+                <MenuItem onClick={() => setRating(rating + 3)}>Burned</MenuItem>
               </Menu>
             </React.Fragment>
           )}
         </PopupState>
         <FontAwesomeIcon icon={faFireFlameCurved} size="md" />
-        <b id="rating">7</b>
+        <b id="rating">{rating}</b>
       </div>
     </div>
-
   );
 };
-
-const Rating = () => {
-  const intialRating = 5;
-  return (
-    <div className="testingClass">
-      <Rater initialRating={intialRating} />
-    </div>
-  )
-}
 
 export default Rating;
 
