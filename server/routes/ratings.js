40b8c8301c9bf86a738c7b950884c3dc22212298
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-07-05 15:59:29 -0400

    comment rating update mongo db bug fix

--- server/routes/ratings.js
+++ server/routes/ratings.js
@@ -8,21 +8,19 @@ const ObjectId = require("mongodb").ObjectId;
 
 //for updating the rating in mongo
 ratingsRoutes.route("/ratings/update").post(function (req, response) {
-    console.log('serving rating route')
-    let db_connect = dbo.getDb();
-    let myobj = {
-      commentRating: req.body.commentRating,
-      _id: ObjectId('62bb4f948f03649b96f3966b')
-    };
-    let filter= myobj._id
-    let updatedRating = {
-        $set:{
-            commentRating: 23
-        },
-    };
-    db_connect.collection("comments").updateOne(filter, updatedRating);
-  });
+  console.log("serving rating route");
+  let db_connect = dbo.getDb();
+  // let myobj = {
+  //   commentRating: req.body.commentRating,
+  // };
+  const filter = { _id: ObjectId("62bb4f948f03649b96f3966b") };
+  const updatedRating = {
+    $set: {
+      commentRating: 23,
+    },
+  };
+  const options = { upsert: true };
+  db_connect.collection("comments").updateOne(filter, updatedRating, options);
+});
 
-
-
-  module.exports = ratingsRoutes;
+module.exports = ratingsRoutes;
