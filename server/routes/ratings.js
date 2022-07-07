77ad52d8c6cf7fff4fe6b4110e1e6cfbc2a3c86f
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-07-07 16:55:19 -0400

    pushing all work for today

--- server/routes/ratings.js
+++ server/routes/ratings.js
@@ -8,17 +8,12 @@ const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
 
 //for updating the rating in mongo
-
 ratingsRoutes.route("/ratings/update").post(function (req, response) {
   let db_connect = dbo.getDb();
-
   const filter = { _id: ObjectId(req.body.commentId) };
-  //Next Steps
-    //Make filter dynamic
-    //set to varable for rating instead on 31
-  //req.body.postID creates new item-> does not update
-  //updating on frontend but not initial feild
-  //useEffect for updating the rating with a depedencie of set rating/rating
+  //Current Status
+  //writes and reads ratings to monogo
+  //Issue is that you can only do it once so I need to find a way to use the updated rating and not the initial rating. Jory suggestd maybe using a useEffect but wasn't sure 
   const updatedRating = {
     $set: {
       commentRating: req.body.commentRating,
@@ -27,6 +22,5 @@ ratingsRoutes.route("/ratings/update").post(function (req, response) {
   const options = { upsert: true };
   db_connect.collection("comments").updateOne(filter, updatedRating, options);
 });
-
 
 module.exports = ratingsRoutes;
