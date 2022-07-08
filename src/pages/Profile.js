48ea1c97914e43e4c036306b7edccd5dcde5c1bb
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-07-08 16:25:50 -0400

    remove hard-coded asset photos from profile code since we are pulling from s3 and mongo

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,32 +1,7 @@
 import { Link } from "react-router-dom";
-import Image from "../assets/gridPhoto.jpg";
-import Image1 from "../assets/gridPhoto1.jpg";
-import Image2 from "../assets/gridPhoto2.jpg";
-import Image3 from "../assets/gridPhoto3.jpg";
-import Image4 from "../assets/gridPhoto4.jpg";
-import Image5 from "../assets/gridPhoto5.jpg";
-import Image6 from "../assets/gridPhoto6.jpg";
-import Image7 from "../assets/gridPhoto7.jpg";
-import Image8 from "../assets/gridPhoto8.jpg";
-import Image9 from "../assets/gridPhoto9.jpg";
-import Image10 from "../assets/gridPhoto10.jpg";
 import ProfileInfo from "../components/ProfileInfo/ProfileInfo";
 import { useEffect, useState } from "react";
 import { AuthState } from "../context/auth/AuthContext";
-
-const profileImages = [
-  { image: Image },
-  { image: Image1 },
-  { image: Image2 },
-  { image: Image3 },
-  { image: Image4 },
-  { image: Image5 },
-  { image: Image6 },
-  { image: Image7 },
-  { image: Image8 },
-  { image: Image9 },
-  { image: Image10 },
-];
 
 const Profile = () => {
   const [profileImagesArray, setProfileImagesArray] = useState([]);
