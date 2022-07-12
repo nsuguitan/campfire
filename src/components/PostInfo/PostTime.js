45f420b176d2ff616c23785a53d736ae865bcabe
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-07-12 17:00:44 -0400

    function to calculate is working

--- src/components/PostInfo/PostTime.js
+++ src/components/PostInfo/PostTime.js
@@ -1,13 +1,34 @@
-const date1 = new Date('2021-05-12T00:00:00.000Z'); // ISO 8601 string
-const date2 = new Date('12 May 2021 01:00:00 UTC+3');
-const diffInMilliseconds = date2 - date1;
-const diffInHours = diffInMilliseconds / 1000 / 60 / 60;
-console.log(diffInHours); 
+const date1 = new Date('2022-07-11T14:06:42.466+00:00'); 
+const currentDate = new Date();
+const differenceInHours = Math.round((currentDate - date1) / 3600000);
+const differenceInMinutes = Math.round((currentDate - date1) / 600000);
+const differenceInDays = Math.round((currentDate - date1) / 86400000 );
+
+console.log(differenceInMinutes, 'time in minutes');
+console.log(differenceInHours, 'time in hours'); 
+console.log(differenceInDays, 'time in days')
+
+const displayedTime = () => {
+    if (differenceInMinutes === 1){
+        return differenceInMinutes + 'minute ago'
+    }else if (differenceInMinutes < 60 && differenceInMinutes !== 1){
+        return differenceInMinutes + ' mintues ago'
+    } else if (differenceInHours === 1  ){
+        return differenceInHours + ' hour ago'
+    }else if (differenceInHours > 1 && differenceInHours < 24){
+        return differenceInHours + ' hours ago'
+    } else if (differenceInDays === 1){
+        return differenceInDays + ' day ago'
+    }else if (differenceInDays > 1){
+        return differenceInDays + ' days ago'
+    }
+}
+console.log(displayedTime())
 
 const PostTime = () => {
     return(
         <div className='postTime'>
-            <p id="postTime">9 hours ago</p>
+            <p id="postTime"></p>
         </div>
     );
 };
