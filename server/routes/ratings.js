f8fe38b2007a4a01aeefb44d582209c7f182ccc7
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-07-12 12:27:47 -0400

    bug fix, get rid of failed to fetch errors on page refresh

--- server/routes/ratings.js
+++ server/routes/ratings.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 
 const ratingsRoutes = express.Router();
@@ -8,20 +7,25 @@ const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
 
 //for updating the rating in mongo
-ratingsRoutes.route("/ratings/update").post(function (req, response) {
+ratingsRoutes.route("/ratings/update").post(function (req, res) {
   let db_connect = dbo.getDb();
   const filter = { _id: ObjectId(req.body.commentId) };
   //Current Status
   //writes and reads ratings to monogo
-  //Issue is that you can only do it once so I need to find a way to use the updated rating and not the initial rating. Jory suggestd maybe using a useEffect but wasn't sure 
+  //Issue is that you can only do it once so I need to find a way to use the updated rating and not the initial rating. Jory suggestd maybe using a useEffect but wasn't sure
   const updatedRating = {
     $set: {
       commentRating: req.body.commentRating,
     },
   };
- 
+
   const options = { upsert: true };
-  db_connect.collection("comments").updateOne(filter, updatedRating, options);
+  db_connect
+    .collection("comments")
+    .updateOne(filter, updatedRating, options, function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
 });
 
 module.exports = ratingsRoutes;
