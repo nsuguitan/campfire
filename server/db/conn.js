c90b13a931c4044bd26d5348ccd89b756dd005be
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-07-18 15:26:01 -0400

    move atlas uri secret from env file to aws secrets manager

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,15 +1,37 @@
-const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI;
+const getAtlasURI = async () => {
+  var AWS = require("aws-sdk"),
+    region = "us-east-1",
+    secretName = "atlas-uri-for-mongo";
+  // Create a Secrets Manager client
+  var awsClient = new AWS.SecretsManager({
+    region: region,
+  });
 
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+  // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  // We rethrow the exception by default.
+
+  return awsClient.getSecretValue({ SecretId: secretName }).promise();
+}; // Load the AWS SDK
+
+const getDbString = async () => {
+  let atlasURI = JSON.parse((await getAtlasURI()).SecretString).ATLAS_URI;
+  return atlasURI;
+};
 
 var _db;
 
 module.exports = {
-  connectToServer: function (callback) {
+  connectToServer: async function (callback) {
+    const { MongoClient } = require("mongodb");
+
+    const Db = await getDbString();
+
+    const client = new MongoClient(Db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     client.connect(function (err, db) {
       if (db) {
         _db = db.db("CampfireDB");
