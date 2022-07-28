7ac32ecaab3eed2631dc69f1114ed376b414112a
Chelsea Merrill <72108067+ChelseaMerrill@users.noreply.github.com>
2022-07-28 10:05:31 -0400

    turned off full width styles

--- src/components/AuthInput/AuthInput.js
+++ src/components/AuthInput/AuthInput.js
@@ -1,20 +1,22 @@
 import { TextField } from "@mui/material";
 import './AuthInput.css'
 
+
 const AuthInput = (props) => {
   return (
     <TextField
-      fullWidth
       className='authText'
       sx={{ marginBottom: "10px", 
         border: '1px solid var(--campfire-gray)',
         backgroundColor: 'var(--campfire-dark-gray)',  
+        color: 'white'
       }}
       label={props.label}
       name={props.name}
       value={props.value}
       size="small"
       onChange={props.onChange}
+      color={props.color}
     />
   );
 };
